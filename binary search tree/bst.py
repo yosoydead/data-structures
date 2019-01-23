123032ad0cfc5b9090e239f004bd0ba6f99bd3af
yosoydead <radu-bogdan@outlook.com>
2019-01-23 20:29:53 +0200

    added the insert method for bst

--- binary search tree/bst.py
+++ binary search tree/bst.py
@@ -6,4 +6,45 @@ class BST:
     #which will be branches/children
     def __init__(self):
         self.root = None
+
+    #method to insert into the tree
+    def insert(self, data):
+        #create a new node
+        newNode = Node(data)
+        #start from the root
+
+        #check if there is a root
+            #if not, the new node becomes the root
+        if self.root == None:
+            self.root = newNode
+        else:
+        #if there is a root
+            #check if the value of the new node is < or > than the root
+            current = self.root
+
+            while True:
+                #if the newNode data is already in the tree
+                if data == current.data:
+                    return None
+            #if it is >
+                #check to see if there is a node to the right
+                    #if there is, move to that node and repeat the steps
+                    #if not, add the new node as the right property
+                if data < current.data:
+                    if current.left == None:
+                        current.left = newNode
+                        return
+                    else:
+                        current = current.left
+            #if it is <
+                #check to see if there is a node to the left
+                    #if there is, move to that node and repeat the steps
+                    #if not, add the new node as the left property
+                elif data > current.data:
+                    if current.right == None:
+                        current.right = newNode
+                        return
+                    else:
+                        current = current.right
+
     
---
+++ binary search tree/main.py
@@ -0,0 +1,6 @@
+from bst import BST
+
+tree = BST()
+tree.insert(10)
+tree.insert(5)
+tree.insert(13)
