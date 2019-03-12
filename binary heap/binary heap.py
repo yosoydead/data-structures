0f2bbb774af739e6119d3fcb23ef30029648cc97
yosoydead <radu-bogdan@outlook.com>
2019-03-12 23:26:27 +0200

    max binary heap remove item

--- binary heap/binary heap.py
+++ binary heap/binary heap.py
@@ -40,6 +40,58 @@ class MaxBinaryHeap:
             #update index to be parentIndex
             index = parentIndex
 
+    def extractMax(self):
+        #swap the first value in the values property with the last one
+        #the max value should be the first element in an array
+        mx = self.values[0]
+        #pop from the values property, so you can return the value at the end
+        end = self.values.pop()
+
+        #run this code if the array is not empty
+        if len(self.values) > 0:
+            self.values[0] = end
+            self.sinkDown()
+        #return the max
+        return mx
+
+    def sinkDown(self):
+        #your parent index starts at 0 (the root)
+        index = 0
+        length = len(self.values)
+        element = self.values[0]
+
+        #loop
+        while True:
+            leftChild = None
+            rightChild = None
+            swap = None
+
+            #find the index of the left child: 2*index+1 (make sure its not out of bounds)
+            left = 2*index+1
+            if left < length:
+                leftChild = self.values[left]
+                if leftChild > element:
+                    swap = left
+
+            #find the index of the right child: 2*index+2 (make sure its not out of bounds)
+            right = 2*index+2
+            if right < length:
+                rightChild = self.values[right]
+                # If both left and right children are larger, swap with the largest child
+                if (swap == None and rightChild > element) or (swap != None and rightChild > leftChild):
+                    swap = right
+
+            #keep looping and swapping until neither child is larger than the element
+            #if no swaps happen, break the loop
+            if swap == None:
+                break
+                
+            self.values[index]= self.values[swap]
+            self.values[swap] = element
+
+            #update index 
+            index = swap
+
 
 
 
@@ -48,7 +100,7 @@ heap.insert(55)
 heap.insert(1)
 heap.insert(45)
 heap.insert(199)
+
 print(heap.values)
-
-
-        
+print(heap.extractMax())
+print(heap.values)
--- binary heap/readme.md
+++ binary heap/readme.md
@@ -16,3 +16,20 @@
             - keep looping as long as the values element at the parentIndex is less than the values element at the child index:
                 - swap the value of the values element at the parentIndex with the value of the element property at the child index
                 - set the index to be the parentIndex and start over
+    2. Removing from the heap
+        - the standard place from where we remove elements is the root. In a MaxBinaryHeap it means removing the maximum value
+        - remove the root
+        - replace with the most recently added
+        - adjust (sink down)
+            * it is a procedure for deleting the root from the heap (extracting the maximum element in a max-heap or the minimum element in a min-heap) and restoring the properties is called **down-heap** (aka bubble-down, sift-down, extract min/max)
+        - Pseudo Code:
+            - swap the first value in the values property with the last one
+            - pop from the values property, so you can return the value at the end
+            - have the new root "sink down" to the correct spot:
+                * your parent index starts at 0 (the root)
+                * find the index of the left child: 2*index+1 (make sure its not out of bounds)
+                * find the index of the right child: 2*index+2 (make sure its not out of bounds)
+                * if the left or right child is greater than the element, swap. If both left and right children are larger, swap with the largest child
+                * the child index you swapped to now becomes the new parent index
+                * keep looping and swapping until neither child is larger than the element
+                * **return the old root !**
